1043d8ebb94b1f66505d267f26eae5da658f4de6
rrmcastro <ricardormcastro@gmail.com>
2023-03-29 07:57:23 -0300

    Fixing commits

--- polls/views.py
+++ polls/views.py
@@ -27,7 +27,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -36,5 +36,5 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing with POST data.
-        # This prevents data form being posted twice if a user hits the Back button.
-        return HttpResponseRedirect((reverse('polls:results', args=(question_id,))))
+        # This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
